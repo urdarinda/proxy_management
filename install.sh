828b0b5695e19bab547db162f206caa8074f5f0f
Jatin Rungta <jatinrungta27@gmail.com>
2017-02-07 18:43:51 +0530

    Basic files

---
+++ install.sh
@@ -0,0 +1,21 @@
+su
+ip route del default gw 172.31.100.1
+ip route del default
+ip route
+yum update
+yum -y install mlocate net-tools firefox vim
+yum -y groupinstall Development Tools
+yum -y groupinstall "X Window System"
+yum -y install gnome-classic-session gnome-terminal control-center liberation-mono-fonts
+unlink /etc/systemd/system/default.target
+ln -sf /lib/systemd/system/graphical.target /etc/systemd/system/default.target
+yum -y install squid
+mkdir /logs/squid
+chown squid:squid /logs/squid
+mkdir /cache/squid
+chown squid:squid /cache/squid
+firewall-cmd --add-port=3128/tcp --permanent
+firewall-cmd --add-port=3128/udp --permanent
+#make sure that the following is done to cache and logs
+#mount options noatime data=writeback noatime
+#/etc/sysconfig/selinux disable
