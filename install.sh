0d082291c25f882dd8b4e204c7af2bfc58f1b248
Jatin Rungta <jatinrungta27@gmail.com>
2017-02-11 23:46:02 +0530

    cleaned install

--- install.sh
+++ install.sh
@@ -1,15 +1,14 @@
-echo -e "Installing System\n\n\n\n"
+echo -e "Installing System\n\n"
 yum -y update
-yum -y install mlocate net-tools firefox vim gedit nmap nano wget httpd
-yum -y install bind-utils
-yum -y groupinstall Development Tools
+yum -y install mlocate net-tools firefox vim gedit nmap nano wget httpd deltarpm bind-utils gd gd-devel perl-GD squid
+yum -y groupinstall "Development Tools"
 yum -y groupinstall "X Window System"
 yum -y install gnome-classic-session gnome-terminal control-center liberation-mono-fonts
 unlink /etc/systemd/system/default.target
 ln -sf /lib/systemd/system/graphical.target /etc/systemd/system/default.target
-yum -y install squid
 
-echo -e "Creating Squid directories\n\n\n\n"
+
+echo -e "Creating Squid directories\n\n"
 mkdir /logs/squid
 chown squid:squid /logs/squid
 mkdir /cache/squid
@@ -18,7 +17,7 @@ mkdir /cache/squid/swap
 chown squid:squid /cache/squid/swap
 systemctl enable squid
 
-echo -e "Setting up ports\n\n\n\n"
+echo -e "Setting up ports\n\n"
 firewall-cmd --add-port=3128/tcp --permanent
 firewall-cmd --add-port=3128/udp --permanent
 firewall-cmd --add-port=3130/tcp --permanent
@@ -26,15 +25,18 @@ firewall-cmd --add-port=3130/udp --permanent
 #make sure that the following is done to cache and logs
 #mount options noatime data=writeback
 
-echo -e "Setting file limits\n\n\n\n"
+echo -e "Setting up Date\n\n"
+date -s "$(wget -qSO- --max-redirect=0 google.com 2>&1 | grep Date: | cut -d' ' -f5-8)Z"
+
+echo -e "Setting file limits\n\n"
 echo "* - nofile 65535">>/etc/security/limits.conf 
 
-echo -e "Disabling SELINUX\n\n\n\n"
-sed  -i 's/enforcing/disabled/' /etc/sysconfig/selinux
-#TODO GRUB DEFAULT
+#echo -e "Disabling SELINUX\n\n"
+#sed  -i 's/enforcing/disabled/' /etc/sysconfig/selinux
+#TODO GRUB DEFAULT ABOVE IS BUGGED
 
-echo -e "Installing latest kernel\n\n\n\n"
+echo -e "Installing latest kernel\n\n"
 rpm --import https://www.elrepo.org/RPM-GPG-KEY-elrepo.org
 rpm -Uvh http://www.elrepo.org/elrepo-release-7.0-2.el7.elrepo.noarch.rpm
-yum --enablerepo=elrepo-kernel install kernel-ml
+yum --enablerepo=elrepo-kernel -y install kernel-ml
 
