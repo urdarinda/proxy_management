eae9d42d787ff14b569854092e46a2615a55d1dd
Jatin Rungta <jatinrungta27@gmail.com>
2017-11-01 14:23:12 +0530

    Functionality to update with ip provided

--- update.sh
+++ update.sh
@@ -2,11 +2,14 @@ if [[ $# -eq 1 ]];
 then
        if [[ $1 =~ ^(([0-9]|[1-9][0-9])|(1[0-9]{2}|2([0-4][0-9]|5[0-5])))\.(([0-9]|[1-9][0-9])|(1[0-9]{2}|2([0-4][0-9]|5[0-5])))$ ]]
        then
-			mv /etc/squid/squid.conf /etc/squid/squid.conf.bak
-			#wget https://raw.githubusercontent.com/urdarinda/proxy_management/master/squid.conf -O /tmp/squid.conf
+       		squidfile=/etc/squid/squid.conf
+			mv $squidfile $squidfile.bak
+			wget https://raw.githubusercontent.com/urdarinda/proxy_management/master/squid.conf -O $squidfile
 			echo "$1"
 			tmp=`echo "$1" | cut -d. -f1``echo "$1" | cut -d. -f2`
 			echo $tmp
+			sed -i s/XXXXXX/$tmp/g $squidfile
+			sed -i s/XXX\.XXX/$1/g $squidfile
 
 		else
               echo "IP not valid"
