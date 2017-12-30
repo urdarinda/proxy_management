24d8cdae770b066808488ae8c120e2615e6dc533
Jatin Rungta <jatinrungta27@gmail.com>
2017-12-30 12:41:11 +0530

    Add proxy list to update

--- update.sh
+++ update.sh
@@ -2,14 +2,16 @@ if [[ $# -eq 1 ]];
 then
        if [[ $1 =~ ^(([0-9]|[1-9][0-9])|(1[0-9]{2}|2([0-4][0-9]|5[0-5])))\.(([0-9]|[1-9][0-9])|(1[0-9]{2}|2([0-4][0-9]|5[0-5])))$ ]]
        then
-       		squidfile=/etc/squid/squid.conf
-			mv $squidfile $squidfile.bak
-			wget https://raw.githubusercontent.com/urdarinda/proxy_management/master/squid.conf -O $squidfile
+			squiddir=/etc/squid
+			giturl=https://raw.githubusercontent.com/urdarinda/proxy_management/master
+			mv $squiddir/squid.conf $squiddir/squid.conf.bak
+			wget $giturl/squid.conf -O $squiddir/squid.conf
+			wget $giturl/proxy_list -O $squiddir/proxy_list
 			echo "$1"
 			tmp=`echo "$1" | cut -d. -f1``echo "$1" | cut -d. -f2`
 			echo $tmp
-			sed -i s/XXXXXX/$tmp/g $squidfile
-			sed -i s/XXX\.XXX/$1/g $squidfile
+			sed -i s/XXXXXX/$tmp/g $squiddir/squid.conf
+			sed -i s/XXX\.XXX/$1/g $squiddir/squid.conf
 			systemctl restart squid
 
 		else
